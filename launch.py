d8e4448ab6eb2fbcea63fdba7054c004510f6b46
Max MARIE <max.marie.4@gmail.com>
2018-09-13 13:15:12 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-ring_size = [26, 41, 51, 61, 68] #Pour avoir Pc = 0.5 pour les differents Q
+ring_size = [26, 41, 51, 61, 68] # To get Pc = 0.5 for each different Q
 list = (1, 2, 3, 4, 5)
 for j in list:
     print("========================= Resilience for Q = " + str(j) + " and ring_size = " + str(ring_size[j-1]) + " - Pc = 0.5 ===========================")
